492fc1eececf03498d65aeb0fa9f228c17402ad3
YUG THUMMAR <147602704+ybthummar@users.noreply.github.com>
2025-06-19 13:32:00 +0530

    Update testimonial.js
    change the testimonial's quote

--- data/testimonial.js
+++ data/testimonial.js
@@ -1,26 +1,26 @@
 export const testimonial = [
   {
     quote:
-      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+      "The AI-powered interview prep boosted my confidence. I feel well-prepared for campus placements next year!",
     author: "Bhavik Koladiya",
     image: "https://drive.google.com/uc?export=view&id=1BaAtaLzSvsGAMBwWWTD1BOGY2m4wXPnm",
-    role: "Student(B.Tech EC)",
+    role: "3rd Year Student (B.Tech EC)",
     company: "Charusat University",
   },
   {
     quote:
-      "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
+      "The industry insights and career guidance helped me plan ahead for internships and future opportunities.",
     author: "Deep Kakadiya",
     image: "https://drive.google.com/uc?export=view&id=1SlvRHpcScskpHbK0HNNXgY3UZ2OcQdQO",
-    role: "Student(B.Tech AIML)",
+    role: "3rd Year Student (B.Tech AIML)",
     company: "Charusat University",
   },
   {
     quote:
-      "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
+      "Thanks to the resume review tools, my CV is now ATS-friendly. I’ve started getting positive responses for internship applications!",
     author: "Sanskruti Kukadiya",
     image: "https://drive.google.com/uc?export=view&id=1M-7FW2k-ZngfM_BRXlKjClz_zZ1UxpLY",
-    role: "Student(B.Tech AIML)",
+    role: "3rd Year Student (B.Tech AIML)",
     company: "Charusat University",
   },
 ];
